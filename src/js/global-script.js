2564c83d1d429f2b8e586a561cca941ec51bb8d6
amstelart <amsteldroid@gmail.com>
2019-08-13 14:06:50 +0300

    add main

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,22 +1,10 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
-
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
-
-// На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   // code
-// });
+$( document ).ready(function() {
+  $('#main-slider').slick({
+    autoplay: true,
+    fade: true,
+    speed: 600,
+    autoplaySpeed: 5000,
+    cssEase: 'linear'
+  });
+});
