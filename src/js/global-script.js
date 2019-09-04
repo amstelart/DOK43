ea7ac8027275647d26d4fdb0e038a211f5afe40f
amstelart <amsteldroid@gmail.com>
2019-09-04 12:27:05 +0300

    .

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -18,4 +18,29 @@ $( document ).ready(function() {
     autoplaySpeed: 5000,
     cssEase: 'linear'
   });
+
+  var t0;
+
+  $('#toTop').hide().on('click', function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:0}, 300);
+  });
+
+  $(window).on('scroll', function(){
+    clearTimeout(t0);
+    t0 = setTimeout(function () {
+        showScrollTopButton();
+      }, 100);
+  });
+
+  showScrollTopButton();
+
+  function showScrollTopButton(){
+    if ( $(document).scrollTop() >= 900 ) {
+      $('#toTop').fadeIn();
+    }
+    else {
+      $('#toTop').fadeOut();
+    }
+  }
 });
